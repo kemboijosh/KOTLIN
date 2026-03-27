cbfe6061e444f97c3cadebe11cfe16611ba5111f
kemboijosh <kemboijoshua883@gmail.com>
2026-03-27 13:56:10 +0300

    kotlin

---
+++ lesson2b.kt
@@ -0,0 +1,13 @@
+fun main() {
+    // CONDITIONAL STATEMENTS
+    //IF CONDITIONAL STATEMENT: it executes a piece of code when the condition evaluates to true
+
+    val number = -20
+
+    if (number > 0) {
+        print("The number is positive.")
+    }
+    else{
+        println("The number is negative")
+    }
+}
---
+++ lesson2c.kt
@@ -0,0 +1,18 @@
+fun main() {
+    // If ... elseif... conditional statements
+    //This is used to evaluate multiple conditions
+    //The else part gets executed when all the conditionsgiven evaluates to false
+
+    val number = 0
+
+    if (number > 0){
+        println(" The number is positive")
+
+    }
+    elseif (number == 0){
+        println("The number is zero")
+    }
+    else{
+        println("The number is negative")
+    }
+}
